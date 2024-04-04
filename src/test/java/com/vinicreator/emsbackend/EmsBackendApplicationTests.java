c4d56d0a238bf847d03a199e1449538e19acb52e
vinicinrn <zviniciusnascimento@gmail.com>
2024-04-04 14:12:24 -0300

    Initial commit

---
+++ src/test/java/com/vinicreator/emsbackend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicreator.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
